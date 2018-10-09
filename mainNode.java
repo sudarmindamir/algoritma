7eb9605931340c4c1bff6428476ec03d24f0c333
sudarminkuh <43722667+sudarminkuh@users.noreply.github.com>
2018-10-09 08:21:24 -0700

    Add files via upload

---
+++ mainNode.java
@@ -0,0 +1,14 @@
+
+public class mainNode {
+
+	public static void main(String[] args) {
+		Node mhluk = new Node();
+		mhluk.setLabel("Sudarmin ");
+		mhluk.setValue(4);
+		
+		
+		System.out.println("Banyak pacar "+ mhluk.getLabel());
+		System.out.println("Jumlah : " + mhluk.getValue());
+	}
+
+}
